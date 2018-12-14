dae2a0fdca492fa0410af151aba502d8962b82a7
vchs <digitect@live.ru>
2018-12-14 20:47:01 +0300

    Добавил новую папку под плагины.

---
+++ plugins/gnome_looking_glass.py
@@ -0,0 +1,20 @@
+"""
+Работа на основе Gnome Looking Glass
+
+
+Based on:
+
+1. https://github.com/adrienverge/gnome-magic-window
+2. https://github.com/CZ-NIC/run-or-raise
+3. https://wiki.gnome.org/Projects/GnomeShell/LookingGlass
+4. https://askubuntu.com/questions/91938/how-can-one-invoke-lg-looking-glass-from-a-command-line
+
+"""
+# Gnome Looking Glass
+# gdbus call --session --dest org.gnome.Shell --object-path /org/gnome/Shell --method org.gnome.Shell.Eval 'ARGS'
+
+# Minimize active window
+# ARGS = global.display.focus_window.minimize();
+
+# Close active window
+# ARGS = global.display.focus_window.delete(global.get_current_time());
