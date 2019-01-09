215b8512015d73716fa6e0a31116795ab6835340
vchs <digitect@live.ru>
2019-01-09 12:19:47 +0300

    Разделил команды поднятия одного окна и множества. Добавил возможность указания заголовка программы, который надо поднять с указанным классом окна.

--- plugins/wmctrl.py
+++ plugins/wmctrl.py
@@ -17,8 +17,8 @@ class Plugin(plugin.Plugin):
     description = 'Useful window management commands on Xorg using the wmctrl. Use "wmctrl -lx" to look names.'
     active_window = None
 
-    def raise_or_run(self,  *args):
-        print('Raise or run: %s' % args)
+    def raise_or_run(self, *args, raise_all=False):
+        print('Raise or run: %s (all=%s)' % (args, raise_all))
         # Разбираем аргументы
         # print('len(locals()=%s' % len(locals()))
         if len(locals())<2:
@@ -27,7 +27,8 @@ class Plugin(plugin.Plugin):
             prog_exec = args[0][0]
             print('prog_exec=%s' % prog_exec)
             window_name = args[0][1]
-            print('window_name=%s' % window_name)
+            window_title = " ".join(args[0][2:])
+            print('window_name=%s, window_title=%s' % (window_name,window_title))
             # Проверяем, есть-ли уже такое окно
 
             # Получаем список всех открытых окон
@@ -39,26 +40,36 @@ class Plugin(plugin.Plugin):
 
             if window_name in str_outdata:
                 # Окно уже запущено. Надо на него переключиться
-                print('We found win "%s and we raise all windows with same name now."' % window_name)
-                # print('We have LG output on search "%s" window: %s' % (window_name,stdoutdata))
-                for line in str_outdata.splitlines():
-                    if window_name in line:
-                        print('Line found: %s' % line)
-                        win_id = line.split()[0]
-                        print('win_id: %s' % win_id)
-                        c="wmctrl -ia %s" % win_id
-                        print('wmctrl command: %s' % c)
-                        subprocess.run(c.split())
-
-                # c = "wmctrl -x -a "+window_name
-                # print('wmctrl command: %s' % c)
-                # subprocess.run(c.split())
+                if raise_all:
+                    print('We found win "%s and we raise all windows with same name now."' % window_name)
+                    # print('We have LG output on search "%s" window: %s' % (window_name,stdoutdata))
+                    for line in str_outdata.splitlines():
+                        if window_name in line:
+                            print('Line found: %s' % line)
+                            win_id = line.split()[0]
+                            print('win_id: %s' % win_id)
+                            c="wmctrl -ia %s" % win_id
+                            print('wmctrl command: %s' % c)
+                            subprocess.run(c.split())
+                else:
+                    if window_title:
+                        print('We found win "%s" and we raise first window with that class and title contains "%s" now.' % (window_name,window_title))
+                        # c = "wmctrl -x -a " + window_name
+                        c = 'wmctrl -a %s -r %s' % (window_name, window_title)
+                    else:
+                        print('We found win "%s and we raise first window with same name now."' % window_name)
+                        c = "wmctrl -x -a "+window_name
+                    print('wmctrl command: %s' % c)
+                    subprocess.run(c.split())
             else:
                 # Если нет - запускаем программу заново
                 print('Window not found. We will run program "%s" again.' % prog_exec)
                 # subprocess.run(prog_exec)
                 # Запускаем как отдельный независимый процесс, и не ждем завершения выполнения.
                 self.exec_detached(prog_exec)
+
+    def raise_all_or_run(self, *args):
+        self.raise_or_run(*args, raise_all=True)
 
     def close(self,  *args):
         print('Close active window')
@@ -74,7 +85,12 @@ class Plugin(plugin.Plugin):
         # Выполняем оригинальный вызов инициации
         super().__init__()
 
-        self.functions.add('raise', 'Raise window or run app. Parameters: COMMAND WINDOW_NAME', self.raise_or_run)
+        self.functions.add('raise',
+                           'Raise first window with defined name or run app. Parameters: COMMAND WINDOW_NAME',
+                           self.raise_or_run)
+        self.functions.add('raise_all',
+                           'Raise all windows with defined name or run app. Parameters: COMMAND WINDOW_NAME',
+                           self.raise_all_or_run)
         self.functions.add('close', 'Close active window', self.close)
         # self.functions.add('minimize', 'Minimize active window', self.minimize)
         # self.functions.print()
